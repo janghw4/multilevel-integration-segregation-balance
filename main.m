bc855ca3cbe0801226108f0392da50be00ca3454
janghw4 <68210398+janghw4@users.noreply.github.com>
2024-04-01 20:47:21 -0400

    Update main.m

--- main.m
+++ main.m
@@ -1,4 +1,7 @@
 addpath(genpath('BCT folder goes here'))
+addpath(genpath('ETC code'))
+
+%% Calculate ISD
 
 % Place your noGSR BOLD data here
 BOLD_data_noGSR = randn(2000,450);
@@ -11,3 +14,20 @@ FC_noGSR = corr(BOLD_data_noGSR);
 FC_GSR = corr(BOLD_data_GSR);           
 
 ISD = ISD_calculation(FC_noGSR,FC_GSR);
+
+%% Calculate metastability
+
+% Your BOLD timeseries goes here
+BOLD_data_noGSR = randn(2000,450);
+window_size = 100;
+
+% Calculate metastability
+metastability = metastability_calculation(BOLD_data_noGSR,window_size);
+
+%% Calculate pattern complexity
+
+% Your BOLD timeseries goes here
+BOLD_data_noGSR = randn(2000,450);
+window_size = 100;
+
+pattern_complexity = pattern_complexity_calculation(BOLD_data_noGSR,window_size);
